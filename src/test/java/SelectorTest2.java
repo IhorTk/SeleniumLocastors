f37c52e67e129a63c00eabe19cf60db783f32c20
IgorTkachenko <igortkachenko987@gmail.com>
2024-06-13 17:28:50 +0200

    ST2

--- src/test/java/SelectorTest2.java
+++ src/test/java/SelectorTest2.java
@@ -8,16 +8,16 @@ import org.openqa.selenium.WebElement;
 import java.util.List;
 
 import static java.lang.System.in;
-
-public class SelectorTest1 extends BaseTest {
+public class SelectorTest2 extends BaseTest{
     @Test
-    public void selectorTest1() throws InterruptedException {
+    public void selectorTest2() throws InterruptedException {
         driver.get("https://demoqa.com/select-menu");
         driver.manage().window().maximize();
-
-        WebElement selectButton =driver.findElement(By.id("selectOne"));
         JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("window.scrollBy(0,600)", "");
+
+        WebElement selectButton =driver.findElement(By.id("selectOne"));
+
         selectButton.click();
         List<WebElement> selectorOne = driver.findElements(By.cssSelector("div[id^=react-select-3-option-0]"));
         System.out.println("selecktorOne.size() = " + selectorOne.size());
@@ -25,20 +25,19 @@ public class SelectorTest1 extends BaseTest {
         for (WebElement sbutton : selectorOne){
             System.out.println("sbutton.getText() = " + sbutton.getText());
         }
-        int idx=0;
-        for (int i = 0; i < selectorOne.size(); i++) {
-            if(selectorOne.get(i).getText().equals("Mr.")){
-                idx=i;
-            }
-        }
-        String elspis = selectorOne.get(idx).getText();
-        selectorOne.get(idx).click();
 
+        for (WebElement sbutton : selectorOne){
+            if (sbutton.getText().equals("Mr.")){
+                sbutton.click();
+            }
+            break;
+        }
+//        String elspis = selectorOne.get().getText();
 
         Thread.sleep(5000);
 
-        Assertions.assertEquals("Mr.",elspis);
-        Assertions.assertEquals("Mr.",elspis);
+//        Assertions.assertEquals("Mr.",selectorOne.get().getText());
+//        Assertions.assertEquals("Mr.",elspis);
 
     }
 }
