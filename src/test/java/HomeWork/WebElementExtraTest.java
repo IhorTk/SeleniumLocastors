99a6851c2d5232e03e1857d24f4b86445e319408
IgorTkachenko <igortkachenko987@gmail.com>
2024-06-18 20:47:40 +0200

    HausaufgabeP1

--- src/test/java/HomeWork/WebElementExtraTest.java
+++ src/test/java/HomeWork/WebElementExtraTest.java
@@ -1,10 +1,13 @@
 package HomeWork;
 
 import TestSeleniumLocator.BaseTest;
+import org.checkerframework.checker.units.qual.A;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 
 import java.util.List;
 
@@ -18,6 +21,7 @@ public class WebElementExtraTest extends BaseTest {
 
         Assertions.assertEquals(6,selectElementsButton.size(), "Должно быть 6 элементов");
         Assertions.assertEquals("Elements", selectElementsButton.get(0).getText(),"Должно быть Elements");
+        driver.quit();
     }
 
     @Test
@@ -27,18 +31,53 @@ public class WebElementExtraTest extends BaseTest {
         // Проверить есть ли на странице и отображается ли элемент меню Gallery, вывести это в консоль
         // Если отображает то ассерт что элементов меню 5, если нет, то 4
         List<WebElement> elementsMenu = driver.findElements(By.cssSelector("li>a[href]"));
-        for(WebElement menu:elementsMenu){
-            if(menu.getText().equals("Elements")){
-
+        for(WebElement menu:elementsMenu) {
+            if (menu.getText().equals("Gallery")) {
+                System.out.println("Элемент меню " + menu.getText() + " найден");
+                Assertions.assertEquals(5, elementsMenu.size(), "Должно быть 5 элементов");
+                break;
             }
         }
+        if(elementsMenu.size()<5) Assertions.assertEquals(4,elementsMenu.size(),"должно быть 4 элемента");
+        System.out.println("elementsMenu.size() = " + elementsMenu.size());
+        driver.quit();
+
     }
 
     @Test
-    public void testCheckBoxSemiAppearance() { // 3
+    public void testCheckBoxSemiAppearance() throws InterruptedException { // 3
         driver.get("https://demoqa.com/checkbox");
         // Раскройте дерево (Home -> Documents) и кликните Office
         // Проверьте, что у оффиса "полная галочка", а у Documents и Home "половинчатая"
+        driver.manage().window().maximize();
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0,600)", "");
+
+        WebElement branchHome = driver.findElement(By.cssSelector(".rct-icon-expand-close"));
+        branchHome.click();
+
+        Thread.sleep(5000);
+        List<WebElement> branchcheckBoxHome = driver.findElements(By.cssSelector("button[aria-label=Toggle]"));
+
+        branchcheckBoxHome.get(2).click();
+
+        WebElement officeButton =driver.findElement(By.cssSelector("label[for=tree-node-office]"));
+        officeButton.click();
+        WebElement homeButtonAssert = driver.findElement(By.id("tree-node-home"));
+        WebElement documentsButtonAssert = driver.findElement(By.id("tree-node-documents"));
+        WebElement officeButtonAssert = driver.findElement(By.id("tree-node-office"));
+
+        Assertions.assertFalse(homeButtonAssert.isSelected(),"Элемент не должен быть выбран");
+        Assertions.assertTrue(homeButtonAssert.isEnabled(),"Элемент должен быть активен");
+        Assertions.assertFalse(documentsButtonAssert.isSelected(),"Элемент не должен быть выбран");
+        Assertions.assertTrue(documentsButtonAssert.isEnabled(),"Элемент должен быть активен");
+        Assertions.assertTrue(officeButtonAssert.isSelected(),"Элемент должен быть выбран");
+        Assertions.assertTrue(officeButtonAssert.isEnabled(),"Элемент должен быть выбран");
+
+//        WebElement branchDocuments = driver.findElement(By.cssSelector())
+
+        driver.quit();
     }
 
 
